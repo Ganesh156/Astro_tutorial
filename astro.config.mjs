19cff0c0af075dfd326f59e1e7b48bdf59cb5846
Ganesh Hegde <ganesh_hegde@Dineroai.com>
2022-09-30 18:11:50 +0530

    First Push

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.gani.tech/',
+  integrations: [sitemap(), react(), image(), tailwind()]
+});
