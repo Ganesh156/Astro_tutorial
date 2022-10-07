d2430f96cffaed1be017f0f5613c4a7c339cc379
Ganesh <ganeshhegde@Ganeshs-MacBook-Air.local>
2022-10-08 01:08:29 +0530

    Deleted unwanted files

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'techpoints.io';
+export const SITE_DESCRIPTION = 'Welcome to techpoints.io';
+export const HOMEPAGE_URL = 'https://www.gani.tech/'
